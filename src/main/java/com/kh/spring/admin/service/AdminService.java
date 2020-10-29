cc426e008994a397a8663d24a7077e166a878726
jinseon <fantasi89@gmail.com>
2020-10-29 21:36:14 +0900

    201029 3rd jinseon's commit.
    1.feed 검색 기능 추가(개인/그룹 구분 X) -> 추후 컬럼 추가시 코드변경 및 추가예정
    2.feed 상태 변경기능 추가
    3.group 상태 변경기능 추가
    4.댓글 상태 변경기능 추가

--- src/main/java/com/kh/spring/admin/service/AdminService.java
+++ src/main/java/com/kh/spring/admin/service/AdminService.java
@@ -2,6 +2,7 @@ package com.kh.spring.admin.service;
 
 import java.util.ArrayList;
 
+import com.kh.spring.feed.model.vo.Feed;
 import com.kh.spring.feed.model.vo.Reply;
 import com.kh.spring.group.model.vo.Group;
 import com.kh.spring.member.model.vo.Member;
@@ -10,30 +11,56 @@ public interface AdminService {
 
 
 
-	/** 1. 회원 리스트 조회
+	/** 1-1. 회원 리스트 조회
 	 * @param m
 	 * @return
 	 */
 	ArrayList<Member> memberSearchList(Member m);
 
-	/** 1-1. 회원 상태 변경
+	/** 1-2. 회원 상태 변경
 	 * @param member
 	 * @return
 	 */
 	int memberStatusChange(Member m);
 	
 
-	/** 2. 그룹 리스트 조회
+	/** 2-1. 그룹 리스트 조회
 	 * @param g
 	 * @return
 	 */
 	ArrayList<Group> groupSearchList(Group g);
 
-	/** 3. 댓글 리스트 조회
+	/** 2-2. 그룹 상태 변경
+	 * @param g
+	 * @return
+	 */
+	int groupStatusChange(Group g);
+
+	/** 3-1. 댓글 리스트 조회
 	 * @param re
 	 * @return
 	 */
+	
+
+	/** 3-2. 댓글 상태 변경
+	 * @param r
+	 * @return
+	 */
+	int replyStatusChange(Reply r);
+
+	
 	ArrayList<Reply> replySearchList(Reply re);
 
+	/** 4-1. 피드 리스트 조회
+	 * @param f
+	 * @return
+	 */
+	ArrayList<Feed> feedSearchList(Feed f);
+
+	/** 4-2. 피드 상태 변경
+	 * @param f
+	 * @return
+	 */
+	int feedStatusChange(Feed f);
 
 }
